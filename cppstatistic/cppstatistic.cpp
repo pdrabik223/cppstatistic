e03b888c33eb18f44e05d9caabdc76c8d9f8e1ee
piotr223 <piotr.drabik@unilodz.eu>
2021-01-02 19:42:28 +0100

    well addet fev lines

--- cppstatistic/cppstatistic.cpp
+++ cppstatistic/cppstatistic.cpp
@@ -2,7 +2,8 @@
 //
 #include "ctx.h"
 #include "cppstatistic.h"
-
+#include <time.h>
+#include <chrono>
 template <class T>
 class Myarray {
 protected:
@@ -73,13 +74,46 @@ public:
 };
 int main()
 {
+	srand(time(NULL));
 	std::cout << "Hello CMake." << std::endl;
-	std::vector<int> vec;
-	Myarray<int> my(30);
+	std::vector<int> *vec;
+	Myarray<int> *my;
+	ctx<int, double> push("create_and_push");
 
-	vec.push_back(10);
+	
 
-	my.push(10);
-	std::cout <<my[0];
+	for (int i = 0; i < 1000; i++) {
+	
+		
+		
+		my = new Myarray<int>(30);
+
+		vec = new std::vector<int>;
+		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+		for (int j = 0; j < i; j++) {
+			my->push(0);
+		}
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+		
+		start = std::chrono::steady_clock::now();
+
+		for (int j = 0; j < i; j++) {
+			vec->push_back(0);
+		}
+		
+		end = std::chrono::steady_clock::now();
+
+
+
+
+		delete[] my;
+		delete[] vec;
+
+		
+
+	}
+
+
 	return 0;
 }
